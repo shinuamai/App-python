56491fdf3433cd7f5a4d740fb9e76d178136d945
shinuamai <laurad00sandovalg@gmail.com>
2023-06-28 14:57:05 -0500

    Handling POST Requests

--- routes.py
+++ routes.py
@@ -8,7 +8,10 @@ import forms
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods = ['GET', 'POST'])
 def about():
     form = forms.addtaskform()
+    if form.validate_on_submit():
+        print("SUBMITTED TITLE", form.title.data)
+        return render_template('about.html', form=form, title=form.title.data)
     return render_template('about.html', form=form)
