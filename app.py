0d4ff02299297c4dc392aeac0864e993852052bb
shinuamai <laurad00sandovalg@gmail.com>
2023-06-28 13:59:17 -0500

    Extending HTML Templates

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('prueba.html', current_title='Custom Title')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == "__main__":
     app.run(debug=True)
