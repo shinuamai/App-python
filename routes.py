67bdf3f2ad38ae36ce95d01ae4dcfbca208e6e33
shinuamai <laurad00sandovalg@gmail.com>
2023-07-06 14:51:49 -0500

    Update templates

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app,db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from models import Task
 from datetime import datetime
 
@@ -19,5 +19,6 @@ def add():
         t = Task(title=form.title.data, date= datetime.utcnow())
         db.session.add(t)
         db.session.commit()
+        flash('Task added to the database')
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
